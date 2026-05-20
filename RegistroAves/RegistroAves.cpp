438a9d3acef3cd4931fb517396b5240e4d688bd9
AndreaCamposJimenez <andrea.campos.jimenez@est.una.ac.cr>
2026-05-20 15:54:40 -0600

    Agregar lógica de Bitacora en main

--- RegistroAves/RegistroAves.cpp
+++ RegistroAves/RegistroAves.cpp
@@ -1,19 +1,20 @@
-// RegistroAves.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
-
 #include <iostream>
+#include "Bitacora.h"
+#include "AnalisisAves.h"
 
-int main()
-{
-    std::cout << "Hello World!\n";
-}
+using namespace EIF201;
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
+int main() {
+    NodoAve* lista = nullptr;
 
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
+    Bitacora bitacora("datos/anillamientos.txt");
+
+    bitacora.registrarAve("TS-0427", "Garza Tigre", 850.5, 'H', "2026-05-20");
+    bitacora.registrarAve("TS-0428", "Ibis Blanco", 600.2, 'M', "2026-05-20");
+
+    int cargadas = bitacora.cargarValidas(lista);
+
+    std::cout << "Aves validas cargadas: " << cargadas << std::endl;
+    std::cout << "Lineas descartadas: " << bitacora.getDescartadas() << std::endl;
+
+    AnalisisAves analisis;
